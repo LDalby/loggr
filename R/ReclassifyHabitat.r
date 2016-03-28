c3504ef8524e5a0bf0955f581d0b35ef94f6a109
LDalby <lars.dalby>
2016-03-28 19:02:53 +0200

    Add hare project to reclassify habitat

--- R/ReclassifyHabitat.r
+++ R/ReclassifyHabitat.r
@@ -3,22 +3,44 @@
 #' Reclassify raw habitat types into few groups
 #'
 #' @param Habitat character The habitat type
+#' @param type character The project - either starling or hare
 #' @export
-ReclassifyHabitat = function(Habitat) {
+ReclassifyHabitat = function(Habitat = NULL, type = NULL) {
+	if(any(is.null(Habitat), is.null(type))){
+		stop('Input parameter missing')
+	}
 	Habitat = stringr::str_trim(Habitat, side = 'both')
-	switch(EXPR = tolower(Habitat),
-		'winter wheat' = 'wintercrop',
-		'winter rye' = 'wintercrop',
-		'bar mark' = 'baresoil',
-		'fold' = 'grazed',
-		'eng/mose' = 'tallgrass',
-		'high grass' = 'tallgrass',
-		'skov' = 'forest',	
-		'spring barley' = 'springcrop',  
-		'spring oat' = 'springcrop',
-		'farm' = 'farm',
-		'grass' = 'grass',
+	if(tolower(type) == 'starling') {
+		switch(EXPR = tolower(Habitat),
+			'winter wheat' = 'wintercrop',
+			'winter rye' = 'wintercrop',
+			'bar mark' = 'baresoil',
+			'fold' = 'grazed',
+			'eng/mose' = 'tallgrass',
+			'high grass' = 'tallgrass',
+			'skov' = 'forest',	
+			'spring barley' = 'springcrop',  
+			'spring oat' = 'springcrop',
+			'farm' = 'farm',
+			'grass' = 'grass',
 		# Default
-		'SomeFunkyCrop')
+			'SomeFunkyCrop')
+	}
+	if(tolower(Habitat) == 'hare'){
+		switch(EXPR = as.character(Habitat),
+			'1' = 'Beans',
+			'2' = 'Oat',
+			'3' = 'Wheat',
+			'4' = 'Rape',
+			'5' = 'WinterBarley',
+			'6' = 'Barley',
+			'7' = 'SetAside',
+			'8' = 'Grasing',
+			'9' = 'HayGrass',
+			'10' = 'Remise',
+			'11' = 'Vildtager',
+			'12' = 'WinterWheat',
+			'13' = 'Stubble')
+	}
 }
 
